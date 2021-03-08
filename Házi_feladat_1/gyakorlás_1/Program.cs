e256cc3a3e7322f66c8a6345fba98adf2d67ed25
BDominik16 <78854621+BDominik16@users.noreply.github.com>
2021-03-08 11:15:15 +0100

    Első_házi

---
+++ Házi_feladat_1/gyakorlás_1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace gyakorlás_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Szemely szemely = new Szemely();
+            Console.WriteLine("Kérem a nevet, súlyt, magasságot");
+            szemely.Nev=Console.ReadLine();
+            szemely.Suly = Convert.ToInt32(Console.ReadLine());
+            szemely.Magassag =Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(szemely.ToString());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Házi_feladat_1/gyakorlás_1/Szemely.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace gyakorlás_1
+{
+    class Szemely
+    {
+        string nev;
+        int suly;
+        double magassag;
+
+        public string Nev { get => nev; set => nev = value; }
+        public int Suly { get => suly; set => suly = value; }
+        public double Magassag { get => magassag; set => magassag = value; }
+
+        public double tti()
+        {
+            return suly / (magassag * magassag);
+        }
+        public string jellemzo()
+        {
+            double seged=this.tti();
+            if (seged < 18.5)
+            {
+                return "Sovány";
+            }
+            else if (seged > 25)
+            {
+                return "Kövér";
+            }
+            else return "Normál";
+        }
+
+
+        public override string ToString()
+        {
+            return $" Név :  {this.nev}, Súly: {this.suly}, Magasság: {this.magassag}, Tti alkat: {this.tti():n4},{this.jellemzo()}";
+        }
+        
+
+
+
+
+    }
+}
